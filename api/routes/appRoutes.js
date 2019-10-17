7e601408432194cc7ea8fdc5294d76758f341201
devs <doc1@gmail.com>
2019-10-17 16:34:57 +1100

    Update appRoutes.js

--- api/routes/appRoutes.js
+++ api/routes/appRoutes.js
@@ -3,6 +3,6 @@ module.exports = function(app) {
   var controller = require('../controllers/appController');
 
   app.route('/api/request')
-    .post(controller.create_api_request);
+    .get(controller.create_api_request);
 
 };
